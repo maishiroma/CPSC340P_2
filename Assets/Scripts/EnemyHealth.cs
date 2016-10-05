3e2849869be997bd9d7c0c30459e3c17841c3f90
shiro105 <shiro105@mail.chapman.edu>
2016-10-05 15:00:59 -0700

    Added Enemy health script

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This controls the enemy health when they get hit.
+public class EnemyHealth : MonoBehaviour {
+
+	public int amountHealth;	//How much health will this enemy have?
+	public string enemyColor;	//What color is associated with the enemy?
+
+	//Called when the enemy gets hit by a complimentry color.
+	public void DepleteHealth()
+	{
+		amountHealth--;
+		if(amountHealth <= 0)
+		{
+			DestroyEnemy();
+		}
+	}
+
+	//This is called to destroy the enemy.
+	void DestroyEnemy()
+	{
+		Destroy(this.gameObject);
+	}
+}
