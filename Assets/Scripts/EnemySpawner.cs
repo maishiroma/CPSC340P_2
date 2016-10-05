9709aef62e4b273580c16c913230384f13714b90
jennifer <jenniferly96@hotmail.com>
2016-10-04 20:30:33 -0700

    added enemy spawner; have not tested - dimensions for spawn needed

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemy_prefab;
+    public float spawn_rate;
+    public float time_interval;
+
+    // range of spawn
+    public int left;
+    public int right;
+    public int top;
+    public int bottom;
+
+	
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", time_interval, spawn_rate);
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemy_prefab, new Vector3(Random.Range(left, right), Random.Range(top, bottom), z_depth), Quaternion.identity);
+    }
+}
