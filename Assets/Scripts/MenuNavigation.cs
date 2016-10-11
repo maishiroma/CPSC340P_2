86f3f70c7ef6c2c163a33c7ee449dbab52c3a7f3
jennifer <jenniferly96@hotmail.com>
2016-10-11 12:02:16 -0700

    script for navigating menu scenes

---
+++ Assets/Scripts/MenuNavigation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuNavigation : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+
+    public void LoadMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("CreditScene");
+    }
+
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+}
