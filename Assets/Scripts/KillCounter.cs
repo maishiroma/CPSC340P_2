3beb0ca55eac25ce8d529895d5972d5766c18cdb
jennifer <jenniferly96@hotmail.com>
2016-10-19 15:54:30 -0700

    updated kill counter to look prettier

--- Assets/Scripts/KillCounter.cs
+++ Assets/Scripts/KillCounter.cs
@@ -6,16 +6,17 @@ public class KillCounter : MonoBehaviour {
     public int kill_count = 0;
     
     // sets values to position counter
-    public int pos_x;
-    public int pos_y;
-    public int screen_w;
-    public int screen_h;
+    // default values set as following
+    public int pos_x = 40;
+    public int pos_y = 285;
+    public int screen_w = 16;
+    public int screen_h = 9;
 
     public GUIStyle gui_style = new GUIStyle();
 
     void Start() {
 
-        gui_style.fontSize = 30;
+        gui_style.fontSize = 52;
         gui_style.font = (Font)Resources.Load("Fonts/Painterz/Painterz.tff");
 
     }
@@ -25,9 +26,10 @@ public class KillCounter : MonoBehaviour {
     }
 
     void OnGUI() {
+        // display kills only when they are obtained
         if (kill_count == 1)
             GUI.Label(new Rect(pos_x, pos_y, screen_w, screen_h), kill_count.ToString() + " KO", gui_style);
-        else
+        else if  (kill_count > 1)
             GUI.Label(new Rect(pos_x, pos_y, screen_w, screen_h), kill_count.ToString() + " KOs", gui_style);
     }
 }
